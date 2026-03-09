b4e8a7f7598c2b0b94dee1a61dbe6d8035a69c1a
hfxz <xuan__zai@outlook.com>
2026-03-09 16:36:31 +0800

    feat: 移除评论功能

--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -1,21 +1,21 @@
 // https://vitepress.dev/guide/custom-theme
-import { h } from 'vue'
-import Theme from 'vitepress/theme'
-import './style.css'
+import { h } from "vue";
+import Theme from "vitepress/theme";
+import "./style.css";
 
-import MyGiscus from './components/MyGiscus.vue'
-import IFrame from './components/IFrame.vue'
+import MyGiscus from "./components/MyGiscus.vue";
+import IFrame from "./components/IFrame.vue";
 
 export default {
   extends: Theme,
   Layout: () => {
     return h(Theme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
-      'doc-after': () => h(MyGiscus)
-    })
+      // 'doc-after': () => h(MyGiscus)
+    });
   },
   enhanceApp({ app, router, siteData }) {
     // register global components
-    app.component('IFrame', IFrame)
-  }
-}
+    app.component("IFrame", IFrame);
+  },
+};
